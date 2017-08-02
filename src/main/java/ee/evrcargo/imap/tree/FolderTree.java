e039a52d396a823202a6f5af12a9706866d8f39f
vadim <vadim.kimlaychuk@evrcargo.ee>
2017-08-02 15:33:58 +0300

    fixed subfolder filtering for FolderTree

--- src/main/java/ee/evrcargo/imap/tree/FolderTree.java
+++ src/main/java/ee/evrcargo/imap/tree/FolderTree.java
@@ -14,7 +14,7 @@ public class FolderTree implements Tree {
     private List<FolderPath> mailboxMap = new ArrayList<>();
     private Configuration conf;
 
-    public FolderTree (Configuration conf) {
+    public FolderTree(Configuration conf) {
         this.conf = conf;
     }
 
@@ -23,6 +23,14 @@ public class FolderTree implements Tree {
         System.out.println("Reading local folder structure...");
         String basePath = conf.getProperty("mailbox.restore.base");
         dumpFolder(basePath, 0);
+
+        // Filter list according to filter patterns
+        String[] patterns = conf.getProperty("mailbox.ignored.folders").split(",");
+        List<String> folderFilters = Arrays.stream(patterns)
+                .map(String::trim)
+                .collect(Collectors.toList());
+
+        mailboxMap.removeIf(path -> folderFilters.contains(path.getPath()));
         return mailboxMap;
     }
 
@@ -33,21 +41,17 @@ public class FolderTree implements Tree {
      */
     private void dumpFolder(String diskFolder, int depth) {
         try {
-            String[] patterns = conf.getProperty("mailbox.ignored.folders").split(",");
-            Set<String> folderFilters = new HashSet<>(Arrays.asList(patterns));
 
             // Check if there are any subfolder to explore
             List<String> existFolders = Files.walk(Paths.get(diskFolder), 1)
                     .skip(1L)
                     .filter(Files::isDirectory)
-                    .filter(p -> !folderFilters.contains(p.toFile().getName().trim())) // Filter ignored folders
                     .map(Path::toFile)
                     .map(File::getName)
                     .collect(Collectors.toList());
 
-
             // Copy to output list
-            for (int i=0; i< existFolders.size(); i++) {
+            for (int i = 0; i < existFolders.size(); i++) {
                 String base = diskFolder.replace(conf.getProperty("mailbox.restore.base"), "");
                 FolderPath fp;
                 if (base.length() == 0) {
